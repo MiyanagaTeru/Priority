e159e1af51973bb0857b549a97cd169b6fd13fe5
Suinan Li <lsuinan@its.jnj.com>
2015-10-26 16:35:39 -0400

    Add all needed widgets

--- app.js
+++ app.js
@@ -1,7 +1,118 @@
 // create module for custom directives
-var app = angular.module('priorityApp', []);
+var app = angular.module('priorityApp', ['d3']);
 
 // controller business logic
-app.controller('AppCtrl', function AppCtrl ($scope) {
+app.controller('mainCtrl', function ($scope) {
+	//dummy data
+	$scope.data =  [
+	{ key: 'sleep', importance: 7, urgency: 9 },
+	{ key: 'eat', importance: 6, urgency: 8  },
+	{ key: 'play', importance: 10, urgency: 3}
+	];
+	$scope.importanceWeight = 8,
+	$scope.urgencyWeight = 2,
 
+	$scope.orderedData = [];
+	var reorderData = function (data,importanceWeight, urgencyWeight) {
+		$scope.orderedData = [];
+		var k =  urgencyWeight/importanceWeight;
+		for (i=0; i < $scope.data.length; i++) {
+			var tempScore = Math.abs(k*$scope.data[i].importance - $scope.data[i].urgency + 10 )/Math.sqrt(k*k+1);
+			console.log(tempScore);
+			var tempObj = { key: $scope.data[i].key, score: tempScore};
+			$scope.orderedData.push(tempObj);
+		}
+	};
+	reorderData($scope.data);
+	$scope.$watch('[data, importanceWeight, urgencyWeight]', function(newVals, oldVals) {
+	  return reorderData(newVals[0], newVals[1], newVals[2]);
+	}, true);
+
+	var updateWeights = function (urgencyWeight) {
+		$scope.importanceWeight = 10.1 - urgencyWeight;
+	}
+	updateWeights($scope.urgencyWeight);
+	$scope.$watch('urgencyWeight', function(newVals, oldVals) {
+	  return updateWeights(newVals);
+	}, true);
 });
+
+app.directive('d3Chart', ['d3Service', function(d3Service) {
+	return {
+		restrict: 'EA',
+		scope: {
+			data: '=',
+			importanceweight: '=',
+			urgencyweight: '='
+		},
+		link: function(scope, element, attrs) {
+			d3Service.d3().then(function(d3) {
+				// our d3 code will go here
+				// Set the dimensions of the canvas / graph
+				var margin = {top: 30, right: 20, bottom: 30, left: 50},
+				canvasWidth = 500,
+				canvasHeight = 500,
+				chartWidth = canvasWidth - margin.left - margin.right,
+				chartHeight = canvasHeight - margin.top - margin.bottom;
+				// Adds the svg canvas
+				var svg = d3.select(element[0])
+	            .append('svg')
+				.attr("width", chartWidth + margin.left + margin.right)
+				.attr("height", chartHeight + margin.top + margin.bottom)
+				.append("g")
+				.attr("transform",
+					"translate(" + margin.left + "," + margin.top + ")");
+
+				var render = function( data, importanceweight, urgencyweight ) {
+					// remove all previous items before render
+					svg.selectAll('*').remove();
+
+					// If we don't pass any data, return out of the element
+					if (!data) return;
+
+					var slope = importanceweight/urgencyweight;
+
+					// Set the ranges
+					var x = d3.scale.linear().domain([0, 10]).range([0, chartWidth]);
+					var y = d3.scale.linear().domain([0, 10]).range([chartHeight, 0]);
+					// Define the axes
+					var xAxis = d3.svg.axis().scale(x)
+						.orient("bottom").ticks(5);
+
+					var yAxis = d3.svg.axis().scale(y)
+						.orient("left").ticks(5);
+
+					svg.selectAll("dot")
+						.data(data)
+						.enter().append("circle")
+						.attr("r", 3.5)
+						.attr("cx", function(datum) { return x(datum.importance); })
+						.attr("cy", function(datum) { return y(datum.urgency); });
+					svg.selectAll("label")
+						.data(data)
+						.enter().append("text")
+						.attr("x", function(datum) { return x(datum.importance); })
+						.attr("y", function(datum) { return y(datum.urgency)+20; })
+						.attr("text-anchor", "middle")
+						.text( function(datum) { return datum.key; });
+
+						// Add the X Axis
+						svg.append("g")
+						.attr("class", "x axis")
+						.attr("transform", "translate(0," + chartHeight + ")")
+						.call(xAxis);
+
+						// Add the Y Axis
+						svg.append("g")
+						.attr("class", "y axis")
+						.call(yAxis);
+				};
+				render(scope.data, scope.importanceweight, scope.urgencyweight );
+				scope.$watch('[data, importanceweight, urgencyweight]', function(newVals, oldVals) {
+				  return render(newVals[0], newVals[1], newVals[2]);
+				}, true);
+			});
+		}
+	};
+}]);
+
